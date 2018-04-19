eb7f320df6f44878c32e16ced8505870bb73195c
root <igor.corralesalonso@telefonica.com>
2018-04-19 11:49:43 +0200

    subimos los tres archivos del tercer ejemplo

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/REPOSITORIOS_GIT/primeros_pasos/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/bin/toolium.bat
@@ -0,0 +1,5 @@
+@echo off
+:: Copyright (c) Telefonica Digital.
+:: CDO QA Team <qacdo@telefonica.com>
+
+python -m toolium11paths %*
